69326a4c289ab95e73d79405dab7e3523342e3b2
Luo Yu <hl1282456555@outlook.com>
2020-10-18 19:09:06 +0800

    Initial commit

---
+++ Source/WalkSimulator/WalkSimulatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WalkSimulatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WALKSIMULATOR_API AWalkSimulatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
