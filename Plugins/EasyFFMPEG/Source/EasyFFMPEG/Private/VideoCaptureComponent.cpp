61654a50dc50cdb0d57897b21e17fff4c1567de1
OtakuLuo <hl1282456555@outlook.com>
2020-10-30 18:43:21 +0800

    Update
    填充InitCapture内容 初始化Codec和CodecContext

--- Plugins/EasyFFMPEG/Source/EasyFFMPEG/Private/VideoCaptureComponent.cpp
+++ Plugins/EasyFFMPEG/Source/EasyFFMPEG/Private/VideoCaptureComponent.cpp
@@ -3,8 +3,18 @@
 
 #include "VideoCaptureComponent.h"
 
+#include "EasyFFMPEG.h"
+
+extern "C" {
+#include "libavcodec/avcodec.h"
+#include "libavutil/opt.h"
+#include "libavutil/imgutils.h"
+}
+
 // Sets default values for this component's properties
 UVideoCaptureComponent::UVideoCaptureComponent()
+	: CodecName(TEXT("libx264"))
+	, CaptureState(ECaptureState::NotInit)
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -13,6 +23,46 @@ UVideoCaptureComponent::UVideoCaptureComponent()
 	// ...
 }
 
+
+void UVideoCaptureComponent::InitCapture(const FString& InVideoFilename)
+{
+	if (IsInitialized()) {
+		UE_LOG(LogFFmpeg, Warning, TEXT("Please uninitialize capture component before call InitCapture()."));
+		return;
+	}
+
+	VideoFilename = InVideoFilename;
+
+	Codec = avcodec_find_encoder_by_name(TCHAR_TO_UTF8(*CodecName));
+	if (Codec == nullptr) {
+		UE_LOG(LogFFmpeg, Error, TEXT("InitCapture() failed: Codec '%s' not found."), *CodecName);
+		return;
+	}
+
+	CodecContext = avcodec_alloc_context3(Codec);
+	if (CodecContext == nullptr) {
+		UE_LOG(LogFFmpeg, Error, TEXT("InitCapture() failed: Could not allocate video codec context."));
+		return;
+	}
+
+	CodecContext->bit_rate = CaptureConfigs.BitRate;
+	CodecContext->width = CaptureConfigs.Width;
+	CodecContext->height = CaptureConfigs.Height;
+	CodecContext->time_base = (AVRational){CaptureConfigs.FrameRate.Y, CaptureConfigs.FrameRate.X};
+	CodecContext->framerate = (AVRational){ CaptureConfigs.FrameRate.X, CaptureConfigs.FrameRate.Y};
+	CodecContext->gop_size = CaptureConfigs.GopSize;
+	CodecContext->max_b_frames = CaptureConfigs.MaxBFrames;
+	CodecContext->pix_fmt = static_cast<AVPixelFormat>(CaptureConfigs.PixelFormat);
+
+	if (Codec->id == AV_CODEC_ID_H264) {
+		av_opt_set(CodecContext->priv_data, "preset", "slow", 0);
+	}
+}
+
+bool UVideoCaptureComponent::IsInitialized()
+{
+	return CaptureState > ECaptureState::Initialized;
+}
 
 // Called when the game starts
 void UVideoCaptureComponent::BeginPlay()
--- Plugins/EasyFFMPEG/Source/EasyFFMPEG/Public/VideoCaptureComponent.h
+++ Plugins/EasyFFMPEG/Source/EasyFFMPEG/Public/VideoCaptureComponent.h
@@ -6,6 +6,41 @@
 #include "Components/SceneComponent.h"
 #include "VideoCaptureComponent.generated.h"
 
+UENUM(BlueprintType)
+enum class ECaptureState : uint8
+{
+	NotInit = 0,
+	Initialized,
+	Capturing,
+};
+
+USTRUCT(BlueprintType)
+struct FCaptureConfigs
+{
+	GENERATED_USTRUCT_BODY()
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Capture Configs")
+	int32	BitRate;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Capture Configs")
+	int32	Width;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Capture Configs")
+	int32	Height;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Capture Configs")
+	FIntPoint	FrameRate;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Capture Configs")
+	int32	GopSize;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Capture Configs")
+	int32	MaxBFrames;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Capture Configs")
+	int32	PixelFormat;
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class EASYFFMPEG_API UVideoCaptureComponent : public USceneComponent
@@ -16,6 +51,12 @@ public:
 	// Sets default values for this component's properties
 	UVideoCaptureComponent();
 
+	UFUNCTION(BlueprintCallable, Category = "Video Capture")
+	void InitCapture(const FString& InVideoFilename);
+
+	UFUNCTION(BlueprintPure, Category = "Video Capture")
+	bool IsInitialized();
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
@@ -24,5 +65,21 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Video Capture")
+	FString CodecName;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Video Capture")
+	FString	VideoFilename;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Video Capture")
+	FCaptureConfigs	CaptureConfigs;
+
+	ECaptureState CaptureState;
+
+private:
+
+	class AVCodecContext* CodecContext;
+	class AVCodec* Codec;
 };
